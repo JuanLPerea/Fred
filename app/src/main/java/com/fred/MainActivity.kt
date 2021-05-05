805f975e227da17f25c8cc480c2876f8a203d1f6
JuanLPerea <juanluisperea412@gmail.com>
2021-05-05 19:26:40 +0200

    Listeners y Timer

--- app/src/main/java/com/fred/MainActivity.kt
+++ app/src/main/java/com/fred/MainActivity.kt
@@ -1,118 +1,149 @@
 package com.fred
 
+import android.app.Activity
 import android.graphics.*
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.view.KeyEvent
+import android.view.MotionEvent
 import android.view.View
-import android.widget.Button
-import android.widget.EditText
-import android.widget.FrameLayout
 import android.widget.ImageView
+import android.widget.RelativeLayout
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var botonArriba: FloatingActionButton
+    lateinit var botonIzquierda: FloatingActionButton
+    lateinit var botonAbajo: FloatingActionButton
+    lateinit var botonDerecha: FloatingActionButton
+    lateinit var miLaberinto: Laberinto
+    var cX = 0
+    var cY = 0
+    var pasoX = 0
+    var pasoY = 0
+    var pulsado_abajo = false
+    var pulsado_arriba = false
+    var pulsado_izquierda = false
+    var pulsado_derecha = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         supportActionBar?.hide()
 
-        val botonArriba = findViewById(R.id.flecha_arriba) as FloatingActionButton
-        val botonIzquierda = findViewById(R.id.flecha_izquierda) as FloatingActionButton
-        val botonAbajo = findViewById(R.id.flecha_abajo) as FloatingActionButton
-        val botonDerecha = findViewById(R.id.flecha_derecha) as FloatingActionButton
+        botonArriba = findViewById(R.id.flecha_arriba)
+        botonIzquierda = findViewById(com.fred.R.id.flecha_izquierda)
+        botonAbajo = findViewById(R.id.flecha_abajo)
+        botonDerecha = findViewById(com.fred.R.id.flecha_derecha)
 
-        val miLaberinto = Laberinto()
+        miLaberinto = Laberinto()
         miLaberinto.generarLaberinto()
 
-        var cX = 0
-        var cY = 0
-        var pasoX = 0
-        var pasoY = -160
-
         // Variables para la posición
-        cY = 34
-        cX = 4
-        crearFondo(cX-4, cY-3, pasoX, pasoY, miLaberinto)
-        Log.d("MyApp", "Pasox: " + pasoX + " PasoY: " + pasoY + "  cX: " + cX + "  cY:" + cY)
+        cX = 0
+        cY = 0
+        pasoX = 0
+        pasoY = -160
 
-        dibujarLaberintoTexto(miLaberinto)
-        /*
+        // Situar al personaje en un punto aleatorio en la fila de mas abajo
+        cY = 34
         do {
             cX = (3..29).shuffled().last()
         } while (miLaberinto.map[cX][cY] != 0)
-        crearFondo(cX-1, cY-1, pasoX, pasoY, miLaberinto)
+        crearFondo(cX - 4, cY - 3, pasoX, pasoY, miLaberinto)
 
-         */
-        botonIzquierda.setOnClickListener {
+        establecerListeners()
 
-            if ((miLaberinto.map[cX-1][cY] == 0 && pasoY == -160) || pasoX != 0) {
-                if (cX > 2) {
-                    pasoX += 32
-                    if (pasoX == 128) {
-                        cX--
-                        pasoX = 0
-                    }
-                    crearFondo(cX-4, cY-3, pasoX, pasoY, miLaberinto)
+        // Con este Timer se actualiza la pantalla cada 1/4 de segundo osea 4 fps!!
+        // lo que le da ese toque tan rítmico que enganchaba en los 80's tic-tac-tic-tac-tic-tac
+        // ¿fundiré el procesador del siglo 21??? sometiéndole a este terrible trabajo :D ???
+
+        val timer = Timer()
+        val ticks = object : TimerTask() {
+            override fun run() {
+                runOnUiThread {
+                    if (pulsado_izquierda) moverFondo(Direcciones.IZQUIERDA)
+                    if (pulsado_derecha) moverFondo(Direcciones.DERECHA)
+                    if (pulsado_arriba) moverFondo(Direcciones.ARRIBA)
+                    if (pulsado_abajo) moverFondo(Direcciones.ABAJO)
+                    crearFondo(cX - 4, cY - 3, pasoX, pasoY, miLaberinto)
                 }
 
             }
-
-            Log.d("MyApp", "Pasox: " + pasoX + " PasoY: " + pasoY + "  cX: " + cX + "  cY:" + cY)
         }
 
-        botonDerecha.setOnClickListener {
-            if ((miLaberinto.map[cX + 1][cY] == 0 && pasoY == -160) || pasoX != 0) {
-                if (cX < 32) {
-                    pasoX -= 32
-                    if (pasoX == -128){
-                        cX++
-                        pasoX = 0
-                    }
-                    crearFondo(cX-4, cY-3, pasoX, pasoY, miLaberinto)
-                }
-            }
-            Log.d("MyApp", "Pasox: " + pasoX + " PasoY: " + pasoY + "  cX: " + cX + "  cY:" + cY)
-        }
-
-        botonArriba.setOnClickListener {
-            if (((miLaberinto.map[cX][cY-1] == 0 || miLaberinto.map[cX][cY-1] == 3) && pasoX == 0) || pasoY != -160) {
-                if (cY > 2){
-                    pasoY += 32
-                    if (pasoY == 0) {
-                        cY--
-                        pasoY = -160
-                    }
-                    crearFondo(cX-4, cY-3, pasoX, pasoY, miLaberinto)
-                }
-
-            }
-            Log.d("MyApp", "Pasox: " + pasoX + " PasoY: " + pasoY + "  cX: " + cX + "  cY:" + cY)
-        }
-
-        botonAbajo.setOnClickListener {
-            if ((miLaberinto.map[cX][cY+1] == 0  && pasoX == 0) || pasoY != -160) {
-                if (cY < 35) {
-                    pasoY -= 32
-                    if (pasoY == -320) {
-                        cY++
-                        pasoY = -160
-                    }
-                    crearFondo(cX-4, cY-3, pasoX, pasoY, miLaberinto)
-                }
-
-            }
-            Log.d("MyApp", "Pasox: " + pasoX + " PasoY: " + pasoY + "  cX: " + cX + "  cY:" + cY)
-        }
-
-
-
+        timer.schedule(ticks, 250, 250)
     }
 
 
-    fun crearFondo(posX: Int, posY: Int, offsetInicialX: Int, offsetInicialY : Int, miLaberinto: Laberinto) {
+    private fun establecerListeners() {
+        botonArriba.setOnTouchListener { v, event ->
+            val action = event.action
+            when (action) {
+                MotionEvent.ACTION_DOWN -> {
+                    pulsado_arriba = true
+                }
+                MotionEvent.ACTION_UP -> {
+                    pulsado_arriba = false
+                }
+
+            }
+            true
+        }
+
+
+        botonAbajo.setOnTouchListener { v, event ->
+            val action = event.action
+            when (action) {
+                MotionEvent.ACTION_DOWN -> {
+                    pulsado_abajo = true
+                }
+                MotionEvent.ACTION_UP -> {
+                    pulsado_abajo = false
+                }
+            }
+            true
+        }
+
+
+        botonDerecha.setOnTouchListener { v, event ->
+            val action = event.action
+            when (action) {
+                MotionEvent.ACTION_DOWN -> {
+                    pulsado_derecha = true
+                }
+                MotionEvent.ACTION_UP -> {
+                    pulsado_derecha = false
+                }
+            }
+            true
+        }
+
+        botonIzquierda.setOnTouchListener { v, event ->
+            val action = event.action
+            when (action) {
+                MotionEvent.ACTION_DOWN -> {
+                    pulsado_izquierda = true
+                }
+                MotionEvent.ACTION_UP -> {
+                    pulsado_izquierda = false
+                }
+            }
+            true
+        }
+    }
+
+
+    fun crearFondo(
+        posX: Int,
+        posY: Int,
+        offsetInicialX: Int,
+        offsetInicialY: Int,
+        miLaberinto: Laberinto
+    ) {
 
         var offsetx = offsetInicialX - 128
         var offsety = offsetInicialY - 80
@@ -132,7 +163,7 @@ class MainActivity : AppCompatActivity() {
         var bitmapFondo = Bitmap.createBitmap(896, 800, Bitmap.Config.ARGB_8888)
         var lienzo = Canvas(bitmapFondo)
         val rectDestino = Rect()
-        rectDestino.set(0,0,128,160)
+        rectDestino.set(0, 0, 128, 160)
 
         // La pantalla en horizontal está dividida en 6 espacios completos y está centrado de forma que se ven 1/2 por cada lado. En horizontal hace scroll en 4 pasos cada uno
         // en vertical son 4 completos y se ve 2 pasos del siguiente. Hace scroll con 5 pasos en vertical
@@ -199,7 +230,8 @@ class MainActivity : AppCompatActivity() {
         fondo.setImageBitmap(bitmapFondo)
     }
 
-    fun dibujarLaberintoTexto (miLaberinto: Laberinto) {
+
+    private fun dibujarLaberintoTexto(miLaberinto: Laberinto) {
         var fila = ""
         for (y in 0..35) {
             for (x in 0..36) {
@@ -213,4 +245,128 @@ class MainActivity : AppCompatActivity() {
             Log.d("Miapp", fila)
         }
     }
+
+    private fun moverFondo(direccion: Direcciones) {
+        when (direccion) {
+            Direcciones.ARRIBA -> {
+                // Mover fondo hacia arriba
+                if (((miLaberinto.map[cX][cY - 1] == 0 || miLaberinto.map[cX][cY - 1] == 3) && pasoX == 0) || pasoY != -160) {
+                    if (cY > 2) {
+                        pasoY += 32
+                        if (pasoY == 0) {
+                            cY--
+                            pasoY = -160
+                        }
+                        crearFondo(cX - 4, cY - 3, pasoX, pasoY, miLaberinto)
+                    }
+                }
+            }
+
+            Direcciones.ABAJO -> {
+                // Mover fondo hacia abajo
+                if ((miLaberinto.map[cX][cY + 1] == 0 && pasoX == 0) || pasoY != -160) {
+                    if (cY < 35) {
+                        pasoY -= 32
+                        if (pasoY == -320) {
+                            cY++
+                            pasoY = -160
+                        }
+                        crearFondo(cX - 4, cY - 3, pasoX, pasoY, miLaberinto)
+                    }
+
+                }
+            }
+
+            Direcciones.DERECHA -> {
+                // Derecha
+                if ((miLaberinto.map[cX + 1][cY] == 0 && pasoY == -160) || pasoX != 0) {
+                    if (cX < 32) {
+                        pasoX -= 32
+                        if (pasoX == -128) {
+                            cX++
+                            pasoX = 0
+                        }
+                        crearFondo(cX - 4, cY - 3, pasoX, pasoY, miLaberinto)
+                    }
+                }
+
+            }
+
+            Direcciones.IZQUIERDA -> {
+                // Izquierda
+                if ((miLaberinto.map[cX - 1][cY] == 0 && pasoY == -160) || pasoX != 0) {
+                    if (cX > 2) {
+                        pasoX += 32
+                        if (pasoX == 128) {
+                            cX--
+                            pasoX = 0
+                        }
+                        crearFondo(cX - 4, cY - 3, pasoX, pasoY, miLaberinto)
+                    }
+
+                }
+            }
+        }
+    }
 }
+
+enum class Direcciones {
+    ARRIBA, ABAJO, IZQUIERDA, DERECHA
+}
+
+/*
+
+  // Mover fondo hacia arriba
+                    if (((miLaberinto.map[cX][cY-1] == 0 || miLaberinto.map[cX][cY-1] == 3) && pasoX == 0) || pasoY != -160) {
+                        if (cY > 2){
+                            pasoY += 32
+                            if (pasoY == 0) {
+                                cY--
+                                pasoY = -160
+                            }
+                            crearFondo(cX-4, cY-3, pasoX, pasoY, miLaberinto)
+                        }
+
+
+                    // Mover fondo hacia abajo
+                    if ((miLaberinto.map[cX][cY + 1] == 0 && pasoX == 0) || pasoY != -160) {
+                        if (cY < 35) {
+                            pasoY -= 32
+                            if (pasoY == -320) {
+                                cY++
+                                pasoY = -160
+                            }
+                            crearFondo(cX - 4, cY - 3, pasoX, pasoY, miLaberinto)
+                        }
+
+                    }
+
+                    // Izquierda
+
+                                     if ((miLaberinto.map[cX - 1][cY] == 0 && pasoY == -160) || pasoX != 0) {
+                        if (cX > 2) {
+                            pasoX += 32
+                            if (pasoX == 128) {
+                                cX--
+                                pasoX = 0
+                            }
+                            crearFondo(cX - 4, cY - 3, pasoX, pasoY, miLaberinto)
+                        }
+
+                    }
+
+
+                    // Derecha
+
+                                    if ((miLaberinto.map[cX + 1][cY] == 0 && pasoY == -160) || pasoX != 0) {
+                        if (cX < 32) {
+                            pasoX -= 32
+                            if (pasoX == -128) {
+                                cX++
+                                pasoX = 0
+                            }
+                            crearFondo(cX - 4, cY - 3, pasoX, pasoY, miLaberinto)
+                        }
+                    }
+
+ */
